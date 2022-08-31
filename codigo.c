efa3b856f2aa016d2ec327569607ca97e39ff4a4
Emanuel-Antonio <102038148+Emanuel-Antonio@users.noreply.github.com>
2022-08-31 18:42:19 -0300

    upgrade while

--- codigo.c
+++ codigo.c
@@ -1,103 +1,127 @@
-#include <stdio.h>    
+#include <stdio.h>
 #include <stdlib.h>
 
-typedef struct no{
+typedef struct no
+{
     int valor;
     struct no *proximo;
-}No;
+} No;
 
-void inserir_elemento_inicio(No **lista, int num){
+void inserir_elemento_inicio(No **lista, int num)
+{
     No *novo = malloc(sizeof(No));
-    if(novo != NULL){
+    if (novo != NULL)
+    {
         novo->valor = num;
         novo->proximo = *lista;
         *lista = novo;
     }
-    else{
+    else
+    {
         printf("Erro ao alocar memoria\n");
     }
 }
 
-void inserir_elemento_final(No **lista, int num){
+void inserir_elemento_final(No **lista, int num)
+{
     No *aux, *novo = malloc(sizeof(No));
-    if(novo != NULL){
+    if (novo != NULL)
+    {
         novo->valor = num;
         novo->proximo = NULL;
-        if(*lista == NULL){
+        if (*lista == NULL)
+        {
             *lista = novo;
         }
-        else{
+        else
+        {
             aux = *lista;
-            while(aux->proximo){
+            while (aux->proximo)
+            {
                 aux = aux->proximo;
-            aux->proximo = novo;
             }
+            aux->proximo = novo;
         }
     }
-    else{
+    else
+    {
         printf("Erro ao alocar memoria\n");
     }
 }
 
-void inserir_elemento_meio(No **lista, int num, int ant){
+void inserir_elemento_meio(No **lista, int num, int ant)
+{
     No *aux, *novo = malloc(sizeof(No));
-    if(novo != NULL){
+    if (novo != NULL)
+    {
         novo->valor = num;
-        if(*lista == NULL){
+        if (*lista == NULL)
+        {
             novo->proximo = NULL;
             *lista = novo;
         }
-        else{
+        else
+        {
             aux = *lista;
-            while(aux->valor != ant && aux->proximo){
+            while (aux->valor != ant && aux->proximo)
+            {
                 aux = aux->proximo;
+            }
             novo->proximo = aux->proximo;
             aux->proximo = novo;
-            }
         }
     }
-    else{
+    else
+    {
         printf("Erro ao alocar memoria\n");
     }
 }
 
-void inserir_elemento_ordenado(No **lista, int num){
+void inserir_elemento_ordenado(No **lista, int num)
+{
     No *aux, *novo = malloc(sizeof(No));
-    if(novo != NULL){
+    if (novo != NULL)
+    {
         novo->valor = num;
-        if(*lista == NULL){
+        if (*lista == NULL)
+        {
             novo->proximo = NULL;
             *lista = novo;
         }
-        else if(novo->valor < (*lista)->valor){
+        else if (novo->valor < (*lista)->valor)
+        {
             novo->proximo = *lista;
             *lista = novo;
         }
-        else{
+        else
+        {
             aux = *lista;
-            while(aux->proximo && novo->valor > aux->proximo->valor){
+            while (aux->proximo && novo->valor > aux->proximo->valor)
+            {
                 aux = aux->proximo;
+            }
             novo->proximo = aux->proximo;
             aux->proximo = novo;
-            }
         }
     }
-    else{
+    else
+    {
         printf("Erro ao alocar memoria");
     }
 }
 
-int main(){
+int main()
+{
     int valor, ant;
     No *lista = NULL;
 
-    //inserir_elemento_inicio(&lista,valor);
+    // inserir_elemento_inicio(&lista,valor);
 
-    //inserir_elemento_final(&lista,valor);
+    // inserir_elemento_final(&lista,valor);
 
-    //inserir_elemento_meio(&lista,valor,ant);
+    // inserir_elemento_meio(&lista,valor,ant);
 
-    //inserir_elemento_ordenado(&lista,valor);
+    // inserir_elemento_ordenado(&lista,valor);
 
     return 0;
 }
